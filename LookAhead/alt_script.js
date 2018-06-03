2e73da4677b613737f75826e3bff5e815e1fe519
Paul Lassen <Paul Lassen>
2018-06-03 12:49:05 +0800

    Final pass

--- LookAhead/alt_script.js
+++ LookAhead/alt_script.js
@@ -1,20 +1,15 @@
-//import Tippy from 'tippy.js';
-
 const maxLen =300
 
 var doc = document.body.getElementsByTagName("a");
 for (var i = 0; i < doc.length; i++) {
-var oHT = "Any text I want number "+i.toString();
-var artex = null;
-var tmp = oHT;
 cont(doc, i)
+
 }
 function cont(docu, it){
 fetch(docu[it].href)
 		.then((resp) => resp.text())
 		.then(function(data) {
       var strng = data.toString();
-			//var para = document.createElement("p")
       artex = strng.substring(strng.indexOf("<p>")+3,strng.indexOf("</p>"));
       var newstr = strng.substring(strng.indexOf("</p>")+3);
       while (artex.length<maxLen) {
@@ -25,12 +20,7 @@ fetch(docu[it].href)
       if(artex[0]!="<"){
         textdat = artex.substring(0,maxLen)+"...\"";
       }
-      //doc[i].innerHTML = artex.substring(0,maxLen)+"...";
-      console.log(textdat)
 			docu[it].title = textdat;
 			return textdat;
 		})
-    //doc[i].title = tmp;
-		//console.log(doc[i].outerHTML)
-		//docu[it].title = txd
 }
