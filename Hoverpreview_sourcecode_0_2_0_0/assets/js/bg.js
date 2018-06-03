317e6b9e186e2aec99467cdb6807717e844f1fe4
Paul Lassen <Paul Lassen>
2018-06-03 09:15:23 +0800

    Final Cleanup

--- Hoverpreview_sourcecode_0_2_0_0/assets/js/bg.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.onInstalled.addListener(function (d) {
-  if(d.reason == 'install') { // init needed data
-    chrome.storage.local.set({
-      running: true,
-      notAllowed: []
-    });
-  }
-});
--- Hoverpreview_sourcecode_0_2_0_0/new.js
+++
@@ -1,6 +0,0 @@
-x.each({mouseenter:"mouseover"
-mouseleave:"mouseout"}
-
-
-x.fn.extend({hover:function(e
-t){return this.mouseenter(e).mouseleave(t||e)}
